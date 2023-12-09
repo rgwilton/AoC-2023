58dac65ea7b5c0d23a2df1313e62cfbe3dbdcb10
Robert Wilton <robert.public@wilton.org.uk>
2023-12-09 06:58:52 +0000

    Day 9

--- src/main/scala/Exercises/Ex09.scala
+++ src/main/scala/Exercises/Ex09.scala
@@ -1,10 +1,24 @@
 package aoc
 
 object Ex09 extends Exercise:
-  type Common = Iterator[String]
+  type Common = Seq[(Int, Int)]
 
-  def common(input: Iterator[String]) = input
+  // Recursively calculate the first and last values
+  def calcNext(ints: IndexedSeq[Int]): (Int, Int) =
+    if ints.forall(_ == 0) then (0, 0)
+    else
+      val diffs =
+        ints.sliding(2).map:
+          case Seq(a, b) => b - a
+        .toIndexedSeq
+      val (fi, li) = calcNext(diffs)
+      (ints.head - fi, ints.last + li)
 
-  def part1(races: Common) = 0
+  def common(input: Iterator[String]) = 
+    input.map: line =>
+      line.split(" ").map(_.toInt).toIndexedSeq
+    .toSeq
+    .map(calcNext)
 
-  def part2(races: Common) = 0
+  def part1(answers: Common) = answers.map(_._2).sum
+  def part2(answers: Common) = answers.map(_._1).sum
--- src/test/scala/Ex09Test.scala
+++ src/test/scala/Ex09Test.scala
@@ -6,5 +6,7 @@ import org.junit.Assert.*
 class Ex09Test extends ExTest(Ex09):
   val testcases = Seq(
     TestCase(
-      """""", 0, 0),
-    TestCase(exInput, 0, 0))
+      """|0 3 6 9 12 15
+         |1 3 6 10 15 21
+         |10 13 16 21 30 45""", 114, 2),
+    TestCase(exInput, 1901217887, 905))
