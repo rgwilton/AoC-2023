fbf19e323788510a712b6ab0338c4b92afc496be
Robert Wilton <robert.public@wilton.org.uk>
2023-12-15 06:53:30 +0000

    Day 15

--- src/main/scala/Exercises/Ex15.scala
+++ src/main/scala/Exercises/Ex15.scala
@@ -1,10 +1,43 @@
 package aoc
 
 object Ex15 extends Exercise:
-  type Common = Iterator[String]
+  type Common = Array[String]
 
-  def common(input: Iterator[String]) = input
+  def common(input: Iterator[String]) = 
+    input.mkString("").split(",")
 
-  def part1(races: Common) = 0
+  def hash(s: String) =
+    var v = 0
+    for c <- s do
+      v = ((v + c) * 17) % 256
+    v
 
-  def part2(races: Common) = 0
+  def part1(steps: Common) = 
+    steps.map(hash).sum
+
+  case class Lens(label: String, power: Int)
+  def part2(steps: Common) =
+    val boxes = Array.fill(256)(List[Lens]())
+    for step <- steps do
+      if step.last == '-' then
+        val label = step.init
+        val h = hash(label)
+        boxes(h) =
+          boxes(h).filterNot(l => l.label == label)
+      else
+        val Array(label, value) = step.split("=")
+        val lens = Lens(label, value.toInt)
+        val h = hash(label)
+        boxes(h) = 
+          val b = boxes(h)
+          b.indexWhere(l => l.label == label)
+          match
+            case -1 => lens :: b
+            case x => 
+              b.take(x) ::: List(lens) ::: b.drop(x + 1)
+    
+    // Calc
+    (for (b, i) <- boxes.zip(Iterator.from(1))
+      (l, j) <- b.reverse.zip(Iterator.from(1)) yield
+        i * j * l.power
+    ).sum
--- src/test/scala/Ex15Test.scala
+++ src/test/scala/Ex15Test.scala
@@ -6,6 +6,6 @@ import org.junit.Assert.*
 class Ex15Test extends ExTest(Ex15):
   val testcases = Seq(
     TestCase(
-      """""", 0, 0),
-    TestCase(exInput, 0, 0))
+      """rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7""", 1320, 145),
+    TestCase(exInput, 505459, 228508))
 
